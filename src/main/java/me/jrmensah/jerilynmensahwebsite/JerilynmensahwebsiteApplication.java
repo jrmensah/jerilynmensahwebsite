76f1a3e1a830eb2875c6e0cc96a27351685c1c7c
jrmensah <jerilynmensah@gmail.com>
2017-12-19 18:50:23 -0500

    Initial commit

---
+++ src/main/java/me/jrmensah/jerilynmensahwebsite/JerilynmensahwebsiteApplication.java
@@ -0,0 +1,12 @@
+package me.jrmensah.jerilynmensahwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerilynmensahwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerilynmensahwebsiteApplication.class, args);
+	}
+}
